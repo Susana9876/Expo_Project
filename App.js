b83c62051a330feb13be6a0a1b8f5e2f2a58c55a
Susana Alba Najera <1fsusanaalbanajera@gmail.com>
2022-09-13 22:14:13 -0500

    Style: First App

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Susana Alba Nájera</Text>
+      <Text>Desarrollo Móvil Integral</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'pink',
   },
 });
